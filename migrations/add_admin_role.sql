fb3eab8bdd39a66af51bc2a69549f482789e3551
jmd-jude <jude.hoffner@gmail.com>
2024-12-15 13:18:15 -0500

    Remove migrations directory and add to gitignore

--- migrations/add_admin_role.sql
+++
@@ -1,5 +0,0 @@
--- Add is_admin column to users table
-ALTER TABLE users ADD COLUMN is_admin BOOLEAN NOT NULL DEFAULT FALSE;
-
--- Set test@test.com as admin
-UPDATE users SET is_admin = TRUE WHERE email = 'test@test.com';
